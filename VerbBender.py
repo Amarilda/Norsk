3fbd92135c21c1b8e56c158fcf81e3f3588a415b
Amarilda <Amarilda@users.noreply.github.com>
2021-01-03 16:44:58 +0100

    Create VerbBender.py

---
+++ VerbBender.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from datetime import date
+import random
+
+verbs = pd.read_excel("verbs.xlsx")
+df = pd.read_excel("VerbBender.xlsx")
+
+def VerbBender():
+    num = random.randrange(0, len(verbs))
+    tenses = ['Infinitive','Present tense','Past tense','Past participle']
+    print(verbs['English'][num])
+
+    counter = 0
+    atbilde = []
+    atbilde.append(date.today().strftime("%d/%m/%Y"))
+    atbilde.append(verbs['English'][num])
+    for i in tenses:
+
+        print(i)
+        answer= input()
+        atbilde.append(answer)
+
+        if verbs[i][num] != answer:
+            print(verbs[i][num])
+            counter+= 1
+    print("You got "+str((1 - counter/4)*100)+"% correct")
+    atbilde.append((1 - counter/4)*100)
+    df.loc[len(df)] = atbilde
+    df.to_excel("VerbBender.xlsx", index = False)
+
+
+VerbBender()
