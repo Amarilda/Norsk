37859fe73f10cadb95f71c2aa9fdebb9dc4689f3
Amarilda <Amarilda@users.noreply.github.com>
2021-01-07 11:31:36 +0100

    Create A2.py

---
+++ A2.py
@@ -0,0 +1,37 @@
+import pandas as pd
+from datetime import date
+import random
+
+verbs = pd.read_excel("verbs.xlsx")
+df = pd.read_excel("VerbBender.xlsx")
+
+#Filter out A2 level verbs
+verbs = verbs[verbs.A2 == "Y"].reset_index()
+
+def VerbBender():
+    
+    for num in range(0, len(verbs)):
+
+        tenses = ['Infinitive','Present tense','Past tense','Past participle']
+        print(verbs['English'][num])
+
+        counter = 0
+        atbilde = []
+        
+        atbilde.append(date.today().strftime("%d/%m/%Y hh:ss"))
+        atbilde.append(verbs['English'][num])
+        
+        for i in tenses:
+            print(i)
+            answer= input()
+            atbilde.append(answer)
+
+            if verbs[i][num] != answer:
+                print(verbs[i][num])
+                counter+= 1
+        print("You got "+str((1 - counter/4)*100)+"% correct")
+        atbilde.append((1 - counter/4)*100)
+        df.loc[len(df)] = atbilde
+        df.to_excel("VerbBender.xlsx", index = False)  
+        
+# You think you have a free will? 
