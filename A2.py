5df29e520b42b05193eb1da73e69991931463efe
Amarilda <Amarilda@users.noreply.github.com>
2021-02-12 10:33:02 +0100

    Update A2.py

--- A2.py
+++ A2.py
@@ -29,24 +29,25 @@ def VBB():
     df, verbs,e = filing()
     perfect10 =ThePerfect10()
     verbs = verbs[((verbs['Ending'].notna()) | (verbs.Irregular == "Y")) & ~verbs['English'].isin(perfect10)].reset_index(drop = True)
-    print(f'days {e}')
+    #print(f'days {e}')
     print(f'Todays mission is {len(verbs[verbs.Ending.notna()])} A2 verbs and {len(verbs[verbs.Irregular == "Y"])} irregular verbs')
     return df, verbs
 
 df, verbs = VBB()
 number = len(verbs)
+chance = 10
 
-while number > 0:   
+while number > 0 and chance > 0:   
     print(number)
     
     bins = []
 
     num = random.randint(0, number-1)
-    print(f'this is random number {num}')
+    #print(f'this is random number {num}')
 
     tenses = ['Infinitive','PresentTense','PastTense','PastParticiple']
     print()
-    print(f"{1/len(verbs):.0%}")
+    #print(f"{1/len(verbs):.0%}")
     print(verbs['English'][num])
 
     counter = 0
@@ -62,7 +63,7 @@ while number > 0:
         atbilde.append(answer)      
         if verbs[i][num] != answer:
             print(verbs[i][num])
-            counter+= 1
+            counter+= 1   
     print(f"You got {(1 - counter/4):.2%}  correct")
     atbilde.append(verbs['Ending'][num])
     atbilde.append(verbs.Irregular [num])
@@ -77,3 +78,8 @@ while number > 0:
     if ((1 - counter/4)*100) == 100.0:
         verbs = verbs.drop(num).reset_index(drop = True)
         number -= 1
+    if ((1 - counter/4)*100) != 100.0:
+        chance-=1
+        print(f'You have {chance} tries left')
+        print()
+        
